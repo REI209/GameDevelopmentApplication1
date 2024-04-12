cc7e0e16215008c2cb1afe9851e2ac1cb767d5fb
REI209 <138412628+REI209@users.noreply.github.com>
2024-04-12 15:50:36 +0900

    サウンド、画像やり直し

--- BombingHunter/Smple/Objects/Player/Player.cpp
+++ BombingHunter/Smple/Objects/Player/Player.cpp
@@ -19,8 +19,8 @@ Player::~Player()
 void Player::Initialize()
 {
 	//画像の読み込み
-	animation[0] = LoadGraph("Resouce/Images/画像/1.png");
-	animation[1] = LoadGraph("Resouce/Images/画像/2.png");
+	animation[0] = LoadGraph("Resource/Images/Tri-pilot/1.png");
+	animation[1] = LoadGraph("Resource/Images/Tri-pilot/2.png");
 
 	//エラーチェック
 	if (animation[0] == -1 || animation[1] == -1)
